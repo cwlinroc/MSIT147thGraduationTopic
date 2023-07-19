65123aa9e66f1a9b5c2012542615e5f9b52ae26f
cwlinroc <129026699+cwlinroc@users.noreply.github.com>
2023-07-19 18:07:10 +0800

    2023.7.19 little change

--- MSIT147thGraduationTopic/Program.cs
+++ MSIT147thGraduationTopic/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using MSIT147thGraduationTopic.EFModels;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<GraduationTopicContext>(
+    option => option.UseSqlServer(builder.Configuration.GetConnectionString("DemoConnection"))
+    );
+
 
 var app = builder.Build();
 
--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -1,20 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using MSIT147.Estore.DataLayer.Dtos;
-using MSIT147.Estore.DataLayer.Repositories;
 
 
-
-var repo = new EmployeeRepository();
-repo.Create(new EmployeeDto
-{
-    EmployeeAccount = "test",
-    EmployeeEmail = "test",
-    EmployeeName = "test",
-    EmployeeAvatarPath = "test",
-    EmployeePassword = "test",
-    EmployeePhone = "test",
-    Permission = 1
-});
 Console.WriteLine("success");
 
