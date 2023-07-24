80763f581f8332ebf4c6c0438ba937b54bbcd321
Ritta8090 <129495537+Ritta8090@users.noreply.github.com>
2023-07-24 18:35:32 +0800

    0724待修改

--- MSIT147thGraduationTopic/Program.cs
+++ MSIT147thGraduationTopic/Program.cs
@@ -41,6 +41,6 @@ app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Evaluations}/{action=Index}/{id?}");
 
 app.Run();
