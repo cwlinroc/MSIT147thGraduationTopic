63798894ee86d943e7ac5658952f8a93998fe8b7
Rhynchium <james8486g@gmail.com>
2023-07-24 11:21:41 +0800

    0724-1120

--- MSIT147thGraduationTopic/Program.cs
+++ MSIT147thGraduationTopic/Program.cs
@@ -41,6 +41,6 @@ app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Merchandises}/{action=Index}/{id?}");
 
 app.Run();
