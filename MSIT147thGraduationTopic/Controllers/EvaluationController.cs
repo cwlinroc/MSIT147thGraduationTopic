8436023947afa2b742ce64d318a9c20863ad0279
Ritta8090 <ritta8090@gmail.com>
2023-07-31 06:59:31 +0800

    待解決 資料寫進資料庫 讀入畫面要抓取星星值

--- MSIT147thGraduationTopic/Controllers/EvaluationController.cs
+++ MSIT147thGraduationTopic/Controllers/EvaluationController.cs
@@ -18,37 +18,46 @@ namespace MSIT147thGraduationTopic.Controllers
 
 
         //以OrderId撈訂單資料
-        public IActionResult EIndex(int OrderId)
+        public IActionResult EIndex(int id)
         {
             //記得刪
-            if (OrderId == 0) { OrderId = 1; }
+            //if (OrderId == 0) { OrderId = 1; }
 
             var model = (from x in _context.EvaluationInputs
-                        where x.OrderId == OrderId
-                        select new EvaluationVM
+                        where x.OrderId == id
+                         select new EvaluationVM
                         {
                             OrderId = x.OrderId,                            
                             MerchandiseId = x.MerchandiseId,
                             MerchandiseName = x.MerchandiseName,
                             SpecId = x.SpecId,
                             SpecName = x.SpecName
-                        }).ToList(); 
-            
+                        }).ToList();
+
+            if (model.Count == 0)
+            {
+                ViewBag.ErrorMessage = $"沒有訂單 ( {id} ) 資料";
+            }
+
             return View(model);
         }
         
         [HttpPost]  
-        public IActionResult EIndex(int OrderId,List<Comments> comments)
+        public IActionResult EIndex(int id, List<Comments> comments)
         {
+            List<Evaluation> datalist = new List<Evaluation>();
             foreach (var item in comments)
             {
                 var data = new Evaluation();
-                data.OrderId = OrderId;
+                data.OrderId = id;
                 data.MerchandiseId = item.MerchandiseId;
                 data.Comment= item.Comment;
                 data.Score = item.Score;
+
+                datalist.Add(data);
             }
-            return View();
+            _context.Evaluations.AddRange(datalist);
+            //_context.Evaluations.Savechange();
         }
 
 
