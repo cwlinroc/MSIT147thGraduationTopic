181980bb42e0fcedad0412b22cce96d4a47506b8
Rhynchium <james8486g@gmail.com>
2023-07-31 09:24:19 +0800

    0731-0920

---
+++ MSIT147thGraduationTopic/Controllers/MallController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MSIT147thGraduationTopic.Controllers
+{
+    public class MallController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
