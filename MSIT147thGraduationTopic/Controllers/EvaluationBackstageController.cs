6f52d242c5d7182041d42cd5b5d8426133383589
Ritta8090 <ritta8090@gmail.com>
2023-08-09 23:51:18 +0800

    0809

--- MSIT147thGraduationTopic/Controllers/EvaluationBackstageController.cs
+++ MSIT147thGraduationTopic/Controllers/EvaluationBackstageController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using MSIT147thGraduationTopic.EFModels;
 using MSIT147thGraduationTopic.Models.ViewModels;
+using System.Collections.Generic;
 using System.Data;
 using System.Drawing.Printing;
 using System.Linq;
@@ -19,9 +21,9 @@ namespace MSIT147thGraduationTopic.Controllers
             _context = context;
         }
 
-        public IActionResult EBIndex(int pageSize = 5, int pageNo = 1)
+        public IActionResult EBIndex(string keyword,int pageSize = 5, int pageNo = 1 )
         {
-            var query = PerformSqlQuery(pageSize, pageNo);
+            var query = PerformSqlQuery(pageSize, pageNo, keyword);
 
             if (query == null)
                 return View(new List<EvaluationVM>());
@@ -30,6 +32,7 @@ namespace MSIT147thGraduationTopic.Controllers
             // 獲取總記錄數
             var totalCount = _context.Evaluations.Count();
             // 傳遞查詢結果和總記錄數到View中
+            ViewBag.keyword = keyword;
             ViewBag.PageNo = pageNo;
             ViewBag.PageSize = pageSize;
             //ViewBag.TotalCount = totalCount;
@@ -78,9 +81,11 @@ namespace MSIT147thGraduationTopic.Controllers
             var totalCount = model.Count();
 
             // 傳遞查詢結果和總記錄數到View中
+            ViewBag.keyword = keyword;
             ViewBag.PageNo = pageNo;
             ViewBag.PageSize = pageSize;
-            ViewBag.TotalCount = totalCount;
+            //ViewBag.TotalCount = totalCount;
+            ViewBag.TotalPage = (totalCount % pageSize) > 0 ? (totalCount / pageSize) + 1 : (totalCount / pageSize);
 
             // 當頁數據
             var currentPageData = model.Skip((pageNo - 1) * pageSize).Take(pageSize).ToList();
@@ -101,24 +106,32 @@ namespace MSIT147thGraduationTopic.Controllers
             }
             return RedirectToAction("EBIndex");
         }
-        private List<EvaluationInput> PerformSqlQuery(int pageSize, int pageNo)
+        private List<EvaluationInput> PerformSqlQuery(int pageSize, int pageNo, string keyword)
         {
-            var sql = @"
-                        DECLARE @pageSize INT, @pageNo INT;
+            var sql = $@"
+                        DECLARE @pageSize INT, @pageNo INT, @keyword NVARCHAR(255);
                         SET @pageSize = @p0;
                         SET @pageNo = @p1;
+                        SET @keyword = {(!string.IsNullOrEmpty(keyword) ? keyword : "NULL")};
                         ;WITH T                               
-                        AS (
-                            SELECT *
-                            FROM EvaluationInput                           
+                        AS (                            
+                                SELECT *
+                                FROM EvaluationInput e
+                                WHERE
+                                @keyword IS NULL OR
+                                e.OrderId LIKE '%' + @keyword + '%' OR
+                                e.MerchandiseName LIKE '%' + @keyword + '%' OR
+                                e.Comment LIKE'%' + @keyword + '%'       
+                                                             
                         )
                         SELECT TotalCount = COUNT(1) OVER (), T.*
                         FROM T
                         ORDER BY EvaluationId DESC
                         OFFSET(@pageNo - 1) * @pageSize ROWS
                         FETCH NEXT @pageSize ROWS ONLY;";
+
             //分頁查詢
-            return _context.EvaluationInputs.FromSqlRaw<EvaluationInput>(sql, pageSize, pageNo).ToList();
+            return _context.EvaluationInputs.FromSqlRaw<EvaluationInput>(sql, pageSize, pageNo, keyword).ToList();
         }
 
 
