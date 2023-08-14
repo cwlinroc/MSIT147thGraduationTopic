1ec4c5533f708e5789cda1989475702d687461b4
Malik <53930315+BOSSLIU0961@users.noreply.github.com>
2023-08-14 13:50:38 +0800

    2023-08-14

--- MSIT147thGraduationTopic/Program.cs
+++ MSIT147thGraduationTopic/Program.cs
@@ -72,6 +72,6 @@ app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=CouponFront}/{action=CouponList}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
