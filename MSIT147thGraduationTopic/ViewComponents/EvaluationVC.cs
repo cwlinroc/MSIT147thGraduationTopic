b15baed3787d087108e6f2db131a23a9890d6c6f
cwlinroc <cwlinroc@gmail.com>
2023-08-15 10:17:58 +0800

    2023.8.15 view component

---
+++ MSIT147thGraduationTopic/ViewComponents/EvaluationVC.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MSIT147thGraduationTopic.EFModels;
+
+namespace MSIT147thGraduationTopic.ViewComponents
+{
+    public class EvaluationVC : ViewComponent
+    {
+
+        private readonly GraduationTopicContext _context;
+
+        public EvaluationVC(GraduationTopicContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            ViewData["age"] = 20;
+            ViewBag.name = "測試";
+            return View();
+        }
+    }
+}
