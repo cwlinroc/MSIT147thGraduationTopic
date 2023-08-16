fccb826487c9737e222b3341a0aad26b4b31af67
cwlinroc <cwlinroc@gmail.com>
2023-08-16 10:08:54 +0800

    2023.8.16 evaluation score bar

--- MSIT147thGraduationTopic/wwwroot/js/statistic/evaluationScores.js
+++ MSIT147thGraduationTopic/wwwroot/js/statistic/evaluationScores.js
@@ -6,31 +6,33 @@ const ctxScore = document.getElementById('evaluationChart').getContext('2d');
 const evaluationChart = new Chart(ctxScore, {
     type: 'bar',
     data: {
-        labels: ['五星', '四星', '三星', '二星', '一星'],
+        labels: ['⭐⭐⭐⭐⭐', '⭐⭐⭐⭐', '⭐⭐⭐', '⭐⭐', '⭐'],
         datasets: [{
             label: '評分統計',
-            data: [0,0,0,0,0],
-            borderWidth: 4,
+            data: [0, 0, 0, 0, 0],
+            borderWidth: 1, //條形圖邊框大小
             backgroundColor: [
-                'rgba(255, 205, 86, 0.7)',
-                'rgba(255, 205, 86, 0.7)',
-                'rgba(255, 205, 86, 0.7)',
-                'rgba(255, 205, 86, 0.7)',
-                'rgba(255, 205, 86, 0.7)',
+                'rgba(255, 205, 86, 1)', //條形圖顏色
+                'rgba(255, 205, 86, 1)',
+                'rgba(255, 205, 86, 1)',
+                'rgba(255, 205, 86, 1)',
+                'rgba(255, 205, 86, 1)',
             ],
             borderColor: [
-                'rgba(255, 205, 86, 0.9)',
-                'rgba(255, 205, 86, 0.9)',
-                'rgba(255, 205, 86, 0.9)',
-                'rgba(255, 205, 86, 0.9)',
-                'rgba(255, 205, 86, 0.9)',
+                'rgba(0, 0, 0, 0.5)', //條形圖邊框顏色
+                'rgba(0, 0, 0, 0.5)',
+                'rgba(0, 0, 0, 0.5)',
+                'rgba(0, 0, 0, 0.5)',
+                'rgba(0, 0, 0, 0.5)',
+                'rgba(0, 0, 0, 0.5)',
             ],
         }]
     },
     options: {
+
+        barThickness: 16, // 條形圖粗度
         indexAxis: 'y',
-        // Elements options apply to all of the options unless overridden in a dataset
-        // In this case, we are setting the border of each horizontal bar to be 2px wide
+
         elements: {
             bar: {
                 borderWidth: 2,
@@ -38,20 +40,35 @@ const evaluationChart = new Chart(ctxScore, {
         },
         responsive: true,
         plugins: {
-            //legend: {
-            //    position: 'right',
-            //},
-            //title: {
-            //    display: true,
-            //    text: '評分統計'
-            //}
+            legend: {
+                display: false
+            },
         },
         scales: {
+            x: {
+                grid: {
+                    display: false, // 隐藏 x 轴的格线
+                    borderColor: 'black'
+                },
+                ticks: {
+                    color: 'black',// 设置 x 轴坐标文字颜色
+                }
+            },
             y: {
-                beginAtZero: true
+                grid: {
+                    display: false, // 隐藏 y 轴的格线
+                    borderColor: 'black'
+                },
+                beginAtZero: true,
+                ticks: {
+                    color: 'rgba(255, 205, 86, 1)', // 设置 x 轴坐标文字颜色
+                    font: {
+                        size: 10 // 设置 y 轴坐标文字大小
+                    }
+                }
             }
         }
-    }    
+    }
 })
 
 //evaluationChart
