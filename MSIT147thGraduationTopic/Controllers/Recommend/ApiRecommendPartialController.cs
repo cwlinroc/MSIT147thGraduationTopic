cd01d189f78fc10acfb9b92b67de275af0e745b5
cwlinroc <cwlinroc@gmail.com>
2023-08-18 17:34:46 +0800

    2023.8.18 partial view cart button

--- MSIT147thGraduationTopic/Controllers/Recommend/ApiRecommendPartialController.cs
+++ MSIT147thGraduationTopic/Controllers/Recommend/ApiRecommendPartialController.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using MSIT147thGraduationTopic.EFModels;
 using MSIT147thGraduationTopic.Models.Dtos.Recommend;
 using MSIT147thGraduationTopic.Models.Services;
+using System.Data;
+using System.Security.Claims;
 
 namespace MSIT147thGraduationTopic.Controllers.Recommend
 {
@@ -34,5 +38,24 @@ namespace MSIT147thGraduationTopic.Controllers.Recommend
         }
 
 
+        [HttpGet("addincart/{specId}")]
+        [Authorize(Roles = "會員")]
+        public async Task<ActionResult<int>> AddInCart(int specId)
+        {
+            int memberId = int.TryParse(HttpContext.User.FindFirstValue("MemberId"), out int tempInt) ? tempInt : -1;
+            if (memberId < 0) return -1;
+
+            var cartitem = await _context.CartItems.FirstOrDefaultAsync(o => o.MemberId == memberId && o.SpecId == specId);
+
+            if (cartitem != null)
+            {
+                cartitem.Quantity++;
+                return await _context.SaveChangesAsync();
+            }
+
+            cartitem = new CartItem { SpecId = specId, MemberId = memberId, Quantity = 1 };
+            _context.CartItems.Add(cartitem);
+            return await _context.SaveChangesAsync();
+        }
     }
 }
